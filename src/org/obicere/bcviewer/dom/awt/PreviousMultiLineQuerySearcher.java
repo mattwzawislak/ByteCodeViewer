a12944935fbf62b12871afc3ab433035ab699b28
Obicere <mattwzawislak@gmail.com>
2015-11-16 23:56:47 -0700

    Added multi line searchers. Need to fill them out. Trying to work out ux issues and an algorithm beforehand.

---
+++ src/org/obicere/bcviewer/dom/awt/PreviousMultiLineQuerySearcher.java
@@ -0,0 +1,13 @@
+package org.obicere.bcviewer.dom.awt;
+
+import org.obicere.bcviewer.dom.Document;
+
+/**
+ * @author Obicere
+ */
+public class PreviousMultiLineQuerySearcher implements QuerySearcher {
+    @Override
+    public QueryResult search(final Document document, final Query query) {
+        return null;
+    }
+}
