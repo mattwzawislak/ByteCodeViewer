22e8d19406b0d45631b04f281bd924336c2b3320
Obicere <mattwzawislak@gmail.com>
2016-01-06 09:53:10 -0700

    Fixed issue. The BEV points to a constant integer and not a string.

--- src/org/obicere/bytecode/viewer/modeler/BooleanElementValueModeler.java
+++ src/org/obicere/bytecode/viewer/modeler/BooleanElementValueModeler.java
@@ -1,6 +1,7 @@
 package org.obicere.bytecode.viewer.modeler;
 
 import org.obicere.bytecode.core.objects.BooleanElementValue;
+import org.obicere.bytecode.core.objects.ConstantInteger;
 import org.obicere.bytecode.viewer.dom.DocumentBuilder;
 
 /**
@@ -9,7 +10,11 @@ public class BooleanElementValueModeler implements Modeler<BooleanElementValue> 
     @Override
     public void model(final BooleanElementValue element, final DocumentBuilder builder) {
         final int constantValueIndex = element.getConstantValueIndex();
-        final String value = builder.getConstantPool().getAsString(constantValueIndex);
-        builder.add(value.equals("true"));
+        // assert by jvms 4.7.16.1 that the constant is an integer
+        final ConstantInteger value = (ConstantInteger) builder.getConstantPool().get(constantValueIndex);
+
+        // 0 == false
+        // 1 == true
+        builder.add(value.getBytes() != 0);
     }
 }
