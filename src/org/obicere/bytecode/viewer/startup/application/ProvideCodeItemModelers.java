310389c2c6aa2c754fdcf472ea7c7a5512237a79
Obicere <mattwzawislak@gmail.com>
2016-02-23 08:23:11 -0700

    Changed nonexistent modelers to be the new empty modeler.

--- src/org/obicere/bytecode/viewer/startup/application/ProvideCodeItemModelers.java
+++ src/org/obicere/bytecode/viewer/startup/application/ProvideCodeItemModelers.java
@@ -4,8 +4,8 @@ import org.obicere.bytecode.core.objects.FrameCodeBlock;
 import org.obicere.bytecode.core.objects.LineCodeBlock;
 import org.obicere.bytecode.core.objects.LocalVariable;
 import org.obicere.bytecode.viewer.context.Domain;
+import org.obicere.bytecode.viewer.modeler.EmptyModeler;
 import org.obicere.bytecode.viewer.modeler.FrameCodeBlockModeler;
-import org.obicere.bytecode.viewer.modeler.LineCodeBlockModeler;
 import org.obicere.bytecode.viewer.modeler.LocalVariableModeler;
 import org.obicere.bytecode.viewer.modeler.ModelerSet;
 import org.obicere.bytecode.viewer.startup.StartUpTask;
@@ -18,7 +18,7 @@ public class ProvideCodeItemModelers implements StartUpTask {
         final ModelerSet modelerSet = domain.getModelers();
 
         modelerSet.add(FrameCodeBlock.IDENTIFIER, new FrameCodeBlockModeler());
-        modelerSet.add(LineCodeBlock.IDENTIFIER, new LineCodeBlockModeler());
+        modelerSet.add(LineCodeBlock.IDENTIFIER, EmptyModeler.getInstance());
 
         modelerSet.add(LocalVariable.IDENTIFIER, new LocalVariableModeler());
     }
