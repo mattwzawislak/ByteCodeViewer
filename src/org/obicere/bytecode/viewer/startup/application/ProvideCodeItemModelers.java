d2b799e503a10c9a24617c633f5e8805a2322730
Obicere <mattwzawislak@gmail.com>
2016-03-26 13:23:52 -0600

    Added new code item modelers.

--- src/org/obicere/bytecode/viewer/startup/application/ProvideCodeItemModelers.java
+++ src/org/obicere/bytecode/viewer/startup/application/ProvideCodeItemModelers.java
@@ -1,12 +1,14 @@
 package org.obicere.bytecode.viewer.startup.application;
 
 import org.obicere.bytecode.core.objects.FrameCodeBlock;
+import org.obicere.bytecode.core.objects.ImplicitCodeBlock;
 import org.obicere.bytecode.core.objects.LineCodeBlock;
 import org.obicere.bytecode.core.objects.LocalVariable;
 import org.obicere.bytecode.core.objects.LocalVariableType;
 import org.obicere.bytecode.viewer.context.Domain;
-import org.obicere.bytecode.viewer.modeler.EmptyModeler;
 import org.obicere.bytecode.viewer.modeler.FrameCodeBlockModeler;
+import org.obicere.bytecode.viewer.modeler.ImplicitCodeBlockModeler;
+import org.obicere.bytecode.viewer.modeler.LineCodeBlockModeler;
 import org.obicere.bytecode.viewer.modeler.LocalVariableModeler;
 import org.obicere.bytecode.viewer.modeler.LocalVariableTypeModeler;
 import org.obicere.bytecode.viewer.modeler.ModelerSet;
@@ -20,7 +22,8 @@ public class ProvideCodeItemModelers implements StartUpTask {
         final ModelerSet modelerSet = domain.getModelers();
 
         modelerSet.add(FrameCodeBlock.IDENTIFIER, new FrameCodeBlockModeler());
-        modelerSet.add(LineCodeBlock.IDENTIFIER, EmptyModeler.getInstance());
+        modelerSet.add(ImplicitCodeBlock.IDENTIFIER, new ImplicitCodeBlockModeler());
+        modelerSet.add(LineCodeBlock.IDENTIFIER, new LineCodeBlockModeler());
 
         modelerSet.add(LocalVariable.IDENTIFIER, new LocalVariableModeler());
         modelerSet.add(LocalVariableType.IDENTIFIER, new LocalVariableTypeModeler());
