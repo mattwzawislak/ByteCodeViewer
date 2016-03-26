e158fa7def5103679bb39e2d0a86cb31ec2f9f66
Obicere <mattwzawislak@gmail.com>
2016-03-26 13:25:37 -0600

    Moved code for frame modeling into a frame modeling class.

--- src/org/obicere/bytecode/viewer/modeler/FrameCodeBlockModeler.java
+++ src/org/obicere/bytecode/viewer/modeler/FrameCodeBlockModeler.java
@@ -5,11 +5,21 @@ import org.obicere.bytecode.viewer.dom.DocumentBuilder;
 
 /**
  */
-public class FrameCodeBlockModeler implements Modeler<FrameCodeBlock> {
+public class FrameCodeBlockModeler extends CodeBlockModeler<FrameCodeBlock> {
     @Override
     public void model(final FrameCodeBlock element, final DocumentBuilder builder) {
 
+        builder.add(element.getName());
+        builder.add(" {");
+        builder.indent();
         builder.newLine();
         builder.model(element.getFrame());
+        builder.newLine();
+
+        modelInstructions(element.getInstructions(), builder);
+
+        builder.unindent();
+        builder.newLine();
+        builder.add("}");
     }
 }
