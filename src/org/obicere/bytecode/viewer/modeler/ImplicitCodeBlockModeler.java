75998b21eefb4393a7513db00fd45d2b0ff99981
Obicere <mattwzawislak@gmail.com>
2016-03-26 13:22:58 -0600

    Added implicit code block and modeler for when the line number table and/or stack map frame table is empty.

---
+++ src/org/obicere/bytecode/viewer/modeler/ImplicitCodeBlockModeler.java
@@ -0,0 +1,14 @@
+package org.obicere.bytecode.viewer.modeler;
+
+import org.obicere.bytecode.core.objects.ImplicitCodeBlock;
+import org.obicere.bytecode.viewer.dom.DocumentBuilder;
+
+/**
+ * @author Obicere
+ */
+public class ImplicitCodeBlockModeler extends CodeBlockModeler<ImplicitCodeBlock> {
+    @Override
+    public void model(final ImplicitCodeBlock element, final DocumentBuilder builder) {
+        modelInstructions(element.getInstructions(), builder);
+    }
+}
