e8006c568568519ae7580148f55d9f4b68a8c9eb
Obicere <mattwzawislak@gmail.com>
2016-03-29 14:22:10 -0600

    Synchronized the class storage.

--- src/org/obicere/bytecode/viewer/configuration/ClassStorage.java
+++ src/org/obicere/bytecode/viewer/configuration/ClassStorage.java
@@ -2,15 +2,15 @@ package org.obicere.bytecode.viewer.configuration;
 
 import org.obicere.bytecode.viewer.context.ClassInformation;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  */
 public class ClassStorage {
 
-    private final Map<String, ClassInformation> cache = new HashMap<>();
+    private final Map<String, ClassInformation> cache = new ConcurrentHashMap<>();
 
     public void publish(final String name, final ClassInformation classInformation) {
         cache.put(name, classInformation);
